be16d51128a2cca56aa46d375bf5624710cd1dc4
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-03-22 00:23:40 +0700

    Fix end position error

--- Assignments/Week_1/bot.py
+++ Assignments/Week_1/bot.py
@@ -5,10 +5,9 @@ a = list(map(int,input().split()))[:n]
 
 def findMaxSub(a, n):
     best = -maxsize - 1
-    start = end = startTemp = endTemp = sum = 0
+    start = end = startTemp = sum = 0
     for i in range(0,n):
         if (a[i] < sum + a[i]):
-            endTemp = i
             sum += a[i]
         else:
             startTemp = i
@@ -16,7 +15,7 @@ def findMaxSub(a, n):
         if (sum > best):
             best = sum
             start = startTemp
-            end = endTemp
+            end = i
     return start + 1, end + 1, best
 
 start, end, maxSub = findMaxSub(a,n)
