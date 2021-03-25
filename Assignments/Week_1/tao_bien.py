edee63a0cf6b53b374257c4ba86e2295c27d2bb5
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-03-26 00:02:48 +0700

    Update source code using matrix

--- Assignments/Week_1/tao_bien.py
+++ Assignments/Week_1/tao_bien.py
@@ -1,3 +1,27 @@
+def matMul(a,b):
+    c = [[0,0],[0,0]]
+    c[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0]
+    c[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1]
+    c[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0]
+    c[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1]
+    return c
+
+def matrixFibonacci(n):
+  a = [[1,1],[1,0]]
+  if (n == 1):
+    return a
+  if (n % 2 == 0):
+    return matMul(matrixFibonacci(n // 2), matrixFibonacci(n // 2))
+  else:
+    return matMul(matMul(matrixFibonacci(n // 2), matrixFibonacci(n // 2)),a)
+
+def fibonacci(n):
+  return matrixFibonacci(n)[0][0]
+
+n,k = map(int,input().split())
+print(fibonacci(2*k)*n % 1000000007)
+
+""" Old method
 n,k = map(int, input().split())
 x = 1
 y = 0
@@ -8,3 +32,4 @@ for i in range(1, k+1):
     y = y_curr
 sum = (x + y) * n % 1000000007
 print(sum)
+"""
