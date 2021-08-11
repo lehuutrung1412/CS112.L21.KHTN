2a110be152dc450100737ed6fd6cad52685ebf57
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-08-11 20:19:19 +0700

    Add project euler #1

---
+++ Practice/ProjectEuler/ProjectEuler1.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    k_of_3 = (n-1) // 3
+    k_of_5 = (n-1) // 5
+    k_of_15 = (n-1) // 15
+    sum_ = k_of_3*(k_of_3+1)*3//2 + k_of_5*(k_of_5+1)*5//2 - k_of_15*(k_of_15+1)*15//2
+    print(sum_)
